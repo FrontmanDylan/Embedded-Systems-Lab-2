94ec03b6bd43acf3f1602329f6f8467b83da476d
Frontman Dylan <98844974+FrontmanDylan@users.noreply.github.com>
2022-10-20 00:28:15 -0400

    Update and rename main.c to Q1.c

--- Q1.c
+++ Q1.c
@@ -21,21 +21,3 @@ int main(void)
 
 	return 0;
 }
-*/
-
-char sum(char A, char B)
-{
-    return A + B;
-}
-
-
-int main(void)
-{
-    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
-
-    char a = 0xAA;                //1010 1010
-    char b = 0xBB;                //1011 1011
-    char c = sum(a,b);
-    return 0;
-}
-
